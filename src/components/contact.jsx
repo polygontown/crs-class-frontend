17952fc9116b250c0d2aa41c932a85c8188e7a57
Devaraj P V <devarajperayil327@gmail.com>
2023-05-09 11:27:02 +0530

    new

---
+++ src/components/contact.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+import "./style.scss";
+import { useFetchDoc } from "../hooks/fetch.hook";
+
+export default function Contact(props) {
+  const [{ apiData }] = useFetchDoc("/get-documents?dname=contacts");
+  let poses = [];
+  if (apiData) {
+    poses = apiData?.map((item) => ({pos: [
+      item.document.split(";lat:")[1].split(";lon:")[0],
+      item.document.split(";lon:")[1].split(";")[0],
+    ],
+    add: item.document.split("add:")[1].split(";ph")[0]
+  }));
+  }
+  const position = [20, 20];
+  let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+  });
+
+  L.Marker.prototype.options.icon = DefaultIcon;
+  return (
+    <div className="contact">
+      <h1>CONTACT US</h1>
+      <div className="map">
+        <div className="map-container">
+          <MapContainer
+            sx={{
+              height: "100%",
+            }}
+            center={position}
+            zoom={1}
+            scrollWheelZoom={false}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            {poses?.map((position, index) => (
+              <Marker key={index} position={position.pos}>
+                <Popup>
+                  {position.add}
+                </Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+        </div>
+      </div>
+      <div className="adds">
+        {apiData?.map((item, index) => (
+          <div key={index} className="add-container">
+            <h5 className="title">
+              {item.title}
+            </h5>
+            <h5 className="rest">Address:</h5>
+            <div style={{
+              wordWrap: "break-word",
+              fontSize: "80%"
+            }}>
+              {item.document.split("add:")[1].split(";ph:")[0]}
+            </div>
+            <div className="ph-mail">
+              <div className="ph">
+                <h5>Phone:</h5>
+                <div>
+                <a href={`tel:${item.document.split(";ph:")[1].split(";mail:")[0]}`} target="_blank" rel="noreferrer">
+                  {item.document.split(";ph:")[1].split(";mail:")[0]}
+                  </a>
+                </div>
+              </div>
+              <div className="mail">
+                <h5>Email</h5>
+                <div>
+                <a href={`mailto:${item.document.split(";mail:")[1].split(";lat:")[0]}`} target="_blank" rel="noreferrer">
+                  {item.document.split(";mail:")[1].split(";lat:")[0]}
+                </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+// {
+//     title: "Corporate Head Quarters",
+//     subtitle: "Place name1",
+//     address: "25807 Westheimer Parkway, Suite # 433, Katy, TX 77494 USA.",
+//     phone: "+1 (346) 348 1363",
+//     email: "info@crsclass.com"
+// },
+// {
+//     title: "Regional Centre Indian Ocean",
+//     subtitle: "Place name2",
+//     address: "1st 5A, Heavenly Plaza, CiviRd, Chembumukku, EdappaKakkanad, Kerala 682021",
+//     phone: "0484 3500685",
+//     email: "info@crsclass.com"
+// },
+// {
+//     title: "Corporate Head Quarters",
+//     subtitle: "Place name3",
+//     address: "India Seaport Airport Road, kalamassery, kochi, kerala, India",
+//     phone: "+91 6282443764",
+//     email: "vishnuur@techbyheart.in"
+// }
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const env = {
+    SERVER_ADD: "https://crs-server-production.up.railway.app"
+}
+
+export default env;
